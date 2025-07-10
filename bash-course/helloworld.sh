a73cd3ca8a3e9569c9626d555a352752c010607a
Luis Ojeda <luisma0330@hotmail.com>
2025-07-09 23:50:41 -0600

    Entendiendo los comentarios en bash

--- bash-course/helloworld.sh
+++ bash-course/helloworld.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
-cat "este es un parrafo agregado con cada ejecucion del script" >> hola.txt
+# Este es un comentario
+echo "este es un parrafo agregado con cada ejecucion del script" >> hola.txt # este es otro comentario
+
+: 'este es un comentario 
+de 2 lineas pero 
+puede llegar a ser un comentario
+mas largo'
