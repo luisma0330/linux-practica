ef5061cf27ace01474eb1af5e82156b351fdb3a5
Luis Ojeda <luisma0330@hotmail.com>
2025-07-13 00:24:31 -0600

    Manejo de errores en bash scripting

---
+++ bash-course/errores.sh
@@ -0,0 +1,7 @@
+#! bin/bash
+
+set -e # En caso de error el script se detiene inmediatamente.
+
+set -x # Muestra la ejecucion de cada linea ejecutada por pantalla.
+
+set -u # Cuando uno de los comandos usa una variable que no esta declarada lo maneja como un error y detiene la ejecucion del programa.
